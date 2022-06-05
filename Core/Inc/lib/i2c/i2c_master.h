686fa72d79629ccf932655656ce09082792b0e9b
Markus Hülß <markus.huelss@devdungeon.de>
2022-06-05 21:25:53 +0200

    format i2c?master.h and set generic stm lib header

--- Core/Inc/lib/i2c/i2c_master.h
+++ Core/Inc/lib/i2c/i2c_master.h
@@ -4,22 +4,22 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	#include "stm32g431xx.h"
+#include "stm32g4xx.h"
 #ifdef __cplusplus
 }
 #endif
 
-#include "i2c_interface.h"
-
-class I2C_Master : public I2C_Interface<I2C_Master> {
-	//uint8_t default_addr = 0x3C;
-
+class I2C_Master {
  public:
-	static void init();
-	static void write(uint8_t addr, uint8_t* data, uint16_t len, bool repeated_start = false);
-	//inline uint8_t writeToDefaultAddr(uint8_t* data, uint16_t len) {this->write(this->default_addr, data, len, false); return 0;}
-	//void setDefaultAddr(uint8_t addr) {this->default_addr = addr; }
-	static void read(uint8_t addr, uint8_t* data, uint16_t len, bool stop_cond = false);
+  static void init();
+  static void write(uint8_t addr,
+                    uint8_t* data,
+                    uint16_t len,
+                    bool repeated_start = false);
+  static void read(uint8_t addr,
+                   uint8_t* data,
+                   uint16_t len,
+                   bool stop_cond = false);
 };
 
 #endif
